f24a00f8e8580cafd61404e1165814abe569d044
Soumen Sardar <59526141+soumen-wad@users.noreply.github.com>
2020-12-02 21:24:19 +0530

    on click add class
    //Header menu onclick add active class with page load
    //html element on click add class

--- Jquery.js
+++ Jquery.js
@@ -1,1 +1,23 @@
 
+//Header menu onclick add active class with page load
+$(function(){
+    var url = window.location;
+    // Will only work if string in href matches with location
+    $('.navbar-nav li.nav-item a[href="'+ url +'"]').parent().addClass('active');
+
+    // Will also work for relative and absolute hrefs
+    $('.navbar-nav li.nav-item a').filter(function() {
+        return this.href == url;
+    }).parent().addClass('active');        
+});
+
+
+//html element on click add class
+jQuery(document).ready(function(){
+    jQuery("#basicsAccordion .box").on("click", function() {
+
+        jQuery("#basicsAccordion .box").removeClass("active");
+        jQuery(this).addClass("active");
+       
+      });
+});
